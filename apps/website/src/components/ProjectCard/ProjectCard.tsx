dac671fb2d2f7ec5da904d497b414d64a2a77629
Jessy Pouliot <jessypouliot98@gmail.com>
2025-01-27 23:57:03 -0500

    portfolio

--- apps/website/src/components/ProjectCard/ProjectCard.tsx
+++ apps/website/src/components/ProjectCard/ProjectCard.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { Card } from "@repo/ui/src/components/Card/Card"
-import { motion, useScroll } from "motion/react";
-import { useRef, useState } from "react";
+import { motion } from "motion/react";
+import { useState } from "react";
 import { Modal } from "@repo/ui/src/components/Modal/Modal";
 
 const img = "https://images.unsplash.com/photo-1547039996-61c1135690c0?q=80&w=2889&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"

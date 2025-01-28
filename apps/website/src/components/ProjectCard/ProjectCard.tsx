99e39c6a3a79c4413965913661e6db18138f7010
Jessy Pouliot <jessypouliot98@gmail.com>
2025-01-27 23:09:14 -0500

    portfolio

--- apps/website/src/components/ProjectCard/ProjectCard.tsx
+++ apps/website/src/components/ProjectCard/ProjectCard.tsx
@@ -4,7 +4,7 @@ import { Card } from "@repo/ui/src/components/Card/Card"
 import { AnimatePresence, motion } from "motion/react";
 import { useState } from "react";
 
-const img = "https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+const img = "https://images.unsplash.com/photo-1547039996-61c1135690c0?q=80&w=2889&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
 export type ProjectCardProps = {
   id: string;
@@ -18,9 +18,10 @@ export function ProjectCard({ id, className }: ProjectCardProps) {
       <Card asChild id={id} className={className}>
         <button type="button" onClick={() => setIsOpen((prev) => !prev)}>
           <motion.div
-            className="ui-aspect-video ui-bg-cover ui-bg-left-top"
+            className="aspect-square bg-cover bg-left-top"
             style={{ backgroundImage: `url(${img})` }}
             layoutId={id}
+            transition={{ duration: 2 }}
           />
         </button>
       </Card>
@@ -30,7 +31,7 @@ export function ProjectCard({ id, className }: ProjectCardProps) {
             <div className="p-8 max-w-screen-md mx-auto">
               <Card className="relative p-4">
                 <motion.div
-                  className="aspect-video bg-cover bg-center"
+                  className="aspect-[21/9] bg-cover bg-center"
                   style={{ backgroundImage: `url(${img})` }}
                   layoutId={id}
                 />

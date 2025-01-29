b1e2f5c761e1205564717529d0bd4ec1fac404b8
Jessy Pouliot <jessypouliot98@gmail.com>
2025-01-29 00:35:57 -0500

    portfolio

--- apps/website/src/parts/Header/Header.tsx
+++ apps/website/src/parts/Header/Header.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { ComponentPropsWithoutRef, useEffect, useLayoutEffect, useRef, useState } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import { clsx } from "clsx";
 import { motion, useMotionValueEvent, useScroll, useTransform, easeIn } from "motion/react";
 
@@ -11,27 +11,29 @@ export type HeaderProps = {
 export function Header({ className }: HeaderProps) {
   const headerRef = useRef<HTMLDivElement>(null);
   const { scrollY } = useScroll();
-  const animatedOpacity = useTransform(scrollY, [0, window.innerHeight], [1, 0.5], {
+  const [colors, setColors] = useState<[a: string, b: string]>(["#000", "#000"]);
+  const animatedOpacity = useTransform(scrollY, [0, window.innerHeight-300], colors, {
     ease: easeIn
   });
-  const colorRef = useRef("#000"/* temp value */);
 
   useMotionValueEvent(animatedOpacity, "change", (current) => {
-    headerRef.current?.style.setProperty("--from", `color-mix(in oklab, ${colorRef.current} ${current * 100}%, transparent)`);
+    headerRef.current?.style.setProperty("--from", current);
   });
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const header = headerRef.current;
     if (!header) return;
-    const value = getComputedStyle(header).getPropertyValue("--color");
-    colorRef.current = value;
+    const a = getComputedStyle(header).getPropertyValue("--from-a");
+    const b = getComputedStyle(header).getPropertyValue("--from-b");
+    setColors([a, b]);
   }, []);
 
   return (
     <motion.header
       ref={headerRef}
       className={clsx(
-        "[--color:theme(color.blue.950)] [--from:var(--color)]",
+        "[--from-a:theme(color.blue.950)] [--from-b:theme(color.blue.600)]",
+        "[--from:var(--from-a)]",
         "bg-gradient-to-b from-(--from) from-90% to-transparent",
         className,
       )}
